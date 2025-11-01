87d5a8b94ecd0fe84b221d7a4c934b6bfd0ac011
LeifVillegas <leifgarrettjason.villegas@gmail.com>
2025-11-01 11:58:13 -0700

    soft eng project

---
+++ src/main/java/com/example/tripto/TripToApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripToApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripToApplication.class, args);
+    }
+
+}
